81e18412a69114f455b57c8b7e58cbc719d9b479
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-05-26 17:15:24 -0600

    tests for merchant index api

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,6 +2,140 @@ require 'rails_helper'
 RSpec.describe 'items'  do
 
   describe "get all items (item index)" do
+    describe "happy path" do
+      it "it can get all  items " do
+        merchant0 = Merchant.create!(name: "alber")
+        merchant1 = Merchant.create!(name: "bob ")
+        merchant2 = Merchant.create!(name: "cat")
+        merchant3 = Merchant.create!(name: "dog")
+
+        item0 = merchant0.items.create!(name: "zero", description: "more", unit_price: 10)
+        item1 = merchant0.items.create!(name: "one", description: "thingy", unit_price: 10)
+        item2 = merchant0.items.create!(name: "two", description: "dohicky", unit_price: 10)
+        item3 = merchant0.items.create!(name: "three", description: "stuff", unit_price: 10)
+        item10 = merchant1.items.create!(name: "ten", description: "more", unit_price: 10)
+        item11 = merchant1.items.create!(name: "eleven", description: "thingy", unit_price: 10)
+        item12 = merchant1.items.create!(name: "twelve", description: "thingy", unit_price: 10)
+        item13 = merchant1.items.create!(name: "thirteen", description: "thingy", unit_price: 10)
+        item20 = merchant2.items.create!(name: "twenty one", description: "thingy", unit_price: 10)
+        item21 = merchant2.items.create!(name: "twenty two", description: "thingy", unit_price: 10)
+        item22 = merchant2.items.create!(name: "twenty three", description: "thingy", unit_price: 10)
+        item23 = merchant2.items.create!(name: "twenty four", description: "thingy", unit_price: 10)
+
+        get '/api/v1/items'
+        items = JSON.parse(response.body)
+
+        expect(items["data"].length).to eq(12)
+
+
+      end
+
+      describe 'can show one page at a time' do
+        it 'first page ' do
+          merchant1 = Merchant.create!(name: "bob ")
+          merchant2 = Merchant.create!(name: "cat")
+          merchant3 = Merchant.create!(name: "dog")
+
+          item0 = merchant0.items.create!(name: "zero", description: "more", unit_price: 10)
+          item1 = merchant0.items.create!(name: "one", description: "thingy", unit_price: 10)
+          item2 = merchant0.items.create!(name: "two", description: "dohicky", unit_price: 10)
+          item3 = merchant0.items.create!(name: "three", description: "stuff", unit_price: 10)
+          item10 = merchant1.items.create!(name: "ten", description: "more", unit_price: 10)
+          item11 = merchant1.items.create!(name: "eleven", description: "thingy", unit_price: 10)
+          item12 = merchant1.items.create!(name: "twelve", description: "thingy", unit_price: 10)
+          item13 = merchant1.items.create!(name: "thirteen", description: "thingy", unit_price: 10)
+          item20 = merchant2.items.create!(name: "twenty one", description: "thingy", unit_price: 10)
+          item21 = merchant2.items.create!(name: "twenty two", description: "thingy", unit_price: 10)
+          item22 = merchant2.items.create!(name: "twenty three", description: "thingy", unit_price: 10)
+          item23 = merchant2.items.create!(name: "twenty four", description: "thingy", unit_price: 10)
+
+          get '/api/v1/merchants?page=1&per_page=5'
+          items = JSON.parse(response.body)
+
+          expect(items["data"].length).to eq(5)
+        end
+
+        it 'second page' do
+          merchant1 = Merchant.create!(name: "bob ")
+          merchant2 = Merchant.create!(name: "cat")
+          merchant3 = Merchant.create!(name: "dog")
+
+          item0 = merchant0.items.create!(name: "zero", description: "more", unit_price: 10)
+          item1 = merchant0.items.create!(name: "one", description: "thingy", unit_price: 10)
+          item2 = merchant0.items.create!(name: "two", description: "dohicky", unit_price: 10)
+          item3 = merchant0.items.create!(name: "three", description: "stuff", unit_price: 10)
+          item10 = merchant1.items.create!(name: "ten", description: "more", unit_price: 10)
+          item11 = merchant1.items.create!(name: "eleven", description: "thingy", unit_price: 10)
+          item12 = merchant1.items.create!(name: "twelve", description: "thingy", unit_price: 10)
+          item13 = merchant1.items.create!(name: "thirteen", description: "thingy", unit_price: 10)
+          item20 = merchant2.items.create!(name: "twenty one", description: "thingy", unit_price: 10)
+          item21 = merchant2.items.create!(name: "twenty two", description: "thingy", unit_price: 10)
+          item22 = merchant2.items.create!(name: "twenty three", description: "thingy", unit_price: 10)
+          item23 = merchant2.items.create!(name: "twenty four", description: "thingy", unit_price: 10)
+
+          get '/api/v1/merchants?page=2er_page=5'
+          items = JSON.parse(response.body)
+
+          expect(items["data"].length).to eq(20)
+        end
+
+        it 'last page can be extra short' do
+          merchant1 = Merchant.create!(name: "bob ")
+          merchant2 = Merchant.create!(name: "cat")
+          merchant3 = Merchant.create!(name: "dog")
+
+          item0 = merchant0.items.create!(name: "zero", description: "more", unit_price: 10)
+          item1 = merchant0.items.create!(name: "one", description: "thingy", unit_price: 10)
+          item2 = merchant0.items.create!(name: "two", description: "dohicky", unit_price: 10)
+          item3 = merchant0.items.create!(name: "three", description: "stuff", unit_price: 10)
+          item10 = merchant1.items.create!(name: "ten", description: "more", unit_price: 10)
+          item11 = merchant1.items.create!(name: "eleven", description: "thingy", unit_price: 10)
+          item12 = merchant1.items.create!(name: "twelve", description: "thingy", unit_price: 10)
+          item13 = merchant1.items.create!(name: "thirteen", description: "thingy", unit_price: 10)
+          item20 = merchant2.items.create!(name: "twenty one", description: "thingy", unit_price: 10)
+          item21 = merchant2.items.create!(name: "twenty two", description: "thingy", unit_price: 10)
+          item22 = merchant2.items.create!(name: "twenty three", description: "thingy", unit_price: 10)
+          item23 = merchant2.items.create!(name: "twenty four", description: "thingy", unit_price: 10)
+
+          get '/api/v1/merchants?page=3'
+          items = JSON.parse(response.body)
+
+          expect(items["data"].length).to eq(2)
+        end
+
+        it 'page number less than 1 returns page 1' do
+          merchant1 = Merchant.create!(name: "bob ")
+          merchant2 = Merchant.create!(name: "cat")
+          merchant3 = Merchant.create!(name: "dog")
+
+          item0 = merchant0.items.create!(name: "zero", description: "more", unit_price: 10)
+          item1 = merchant0.items.create!(name: "one", description: "thingy", unit_price: 10)
+          item2 = merchant0.items.create!(name: "two", description: "dohicky", unit_price: 10)
+          item3 = merchant0.items.create!(name: "three", description: "stuff", unit_price: 10)
+          item10 = merchant1.items.create!(name: "ten", description: "more", unit_price: 10)
+          item11 = merchant1.items.create!(name: "eleven", description: "thingy", unit_price: 10)
+          item12 = merchant1.items.create!(name: "twelve", description: "thingy", unit_price: 10)
+          item13 = merchant1.items.create!(name: "thirteen", description: "thingy", unit_price: 10)
+          item20 = merchant2.items.create!(name: "twenty one", description: "thingy", unit_price: 10)
+          item21 = merchant2.items.create!(name: "twenty two", description: "thingy", unit_price: 10)
+          item22 = merchant2.items.create!(name: "twenty three", description: "thingy", unit_price: 10)
+          item23 = merchant2.items.create!(name: "twenty four", description: "thingy", unit_price: 10)
+
+          first_item = Item.all.first
+          last_item = Item.all[9]
+
+
+          get '/api/v1/merchants?page=0&per_page=10'
+          item = JSON.parse(response.body)
+
+          expect(item["data"].first['id']).to eq(first_item.id.to_s)
+          expect(item["data"].last['id']).to eq(last_item.id.to_s)
+        end
+      end
+    end
+  end
+
+  describe "get one item (item showpage)" do
     it "happy path" do
       merchant1 = Merchant.create!(name: "mel")
       merchant2 = Merchant.create!(name: "har")
@@ -51,15 +185,7 @@ RSpec.describe 'items'  do
     end
   end
 
-  describe "get one item (item showpage)" do
-    it "happy path" do
-    end
-
-    it "sad path" do
-    end
-  end
-
-  describe "get an items merchant" do
+  describe "get an item's merchant" do
     it "happy path" do
     end
 
