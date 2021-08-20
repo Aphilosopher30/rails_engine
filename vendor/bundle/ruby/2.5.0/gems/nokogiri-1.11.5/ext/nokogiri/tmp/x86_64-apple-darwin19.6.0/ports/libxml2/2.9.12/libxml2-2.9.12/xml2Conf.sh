0baadb1f610908a08f39f39f7a084775464de297
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-08-20 16:55:28 -0600

    try this

---
+++ vendor/bundle/ruby/2.5.0/gems/nokogiri-1.11.5/ext/nokogiri/tmp/x86_64-apple-darwin19.6.0/ports/libxml2/2.9.12/libxml2-2.9.12/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/aphilosopher30/Desktop/turing_work/old mods/mod_3/rails_engine/rails-engine/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.11.5/ports/x86_64-apple-darwin19.6.0/libxml2/2.9.12/lib"
+XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/aphilosopher30/Desktop/turing_work/old mods/mod_3/rails_engine/rails-engine/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.11.5/ports/x86_64-apple-darwin19.6.0/libxml2/2.9.12/include/libxml2"
+MODULE_VERSION="xml2-2.9.12"
+
